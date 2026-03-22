5af09ae981d69c0c562d7c3e1dde2f8ae1f00e01
Tag Hunt <tag.hunt@titanobservatory.org>
2026-03-21 20:03:21 -0400

    Fixed player manager

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -2,11 +2,6 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
-    [Header("Ambient Sound Volumes")]
-    [SerializeField] private float outsideSoundVolume = 1.0f;
-    [SerializeField] private float insideSoundVolume = 0.15f;
-    [SerializeField] private Collider insideDetector;
-    public bool inHouse { get; private set; } = false;
     public Plane[] cameraPlanes { get; private set; }
     public DisappearBehavior _disappearComponent;
 
@@ -23,11 +18,6 @@ public class PlayerManager : MonoBehaviour
             PlayerManagerInstance = this;
         }
     }
-    private void Start()
-    {
-        SetInsideState(false);
-    }
-
     private void Update()
     {
         cameraPlanes = GeometryUtility.CalculateFrustumPlanes(Camera.main);
@@ -37,33 +27,11 @@ public class PlayerManager : MonoBehaviour
         }
     }
 
-    /*"if the previous activated trigger was the outside one, and player now triggered inside one, player has just crossed the boundary and is now inside"
-
-   "if the previous activated trigger was the inside one, and player now triggered outside one, player has just crossed the boundary and is now outside"
-
-   "if the last activated trigger is the same as the current one, player hasn't crossed any boundary so inside/outside status hasn't changed. */
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Wacky"))
         {
             _disappearComponent = other.GetComponentInChildren<DisappearBehavior>();
         }
-
-        if (!IsInsideTrigger(other))
-            return;
-
-        if (!inHouse)
-            SetInsideState(true);
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Wacky"))
-        {
-            _disappearComponent = null;
-        }
-
-        else if(other.CompareTag("InsideBox") || other.CompareTag("OutsideBox"))
-            _previousActivatedTrigger = other;
     }
 }
